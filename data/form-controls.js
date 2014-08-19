b6dc747b8827d71060fad570a052351de8aa6eb1
Cameron Bradley <cameron@hooroo.com>
2014-08-19 14:39:04 +1000

    added lastname field

--- data/form-controls.js
+++ data/form-controls.js
@@ -1,3 +1,7 @@
 self.port.on("add-name", function(name){
 	document.getElementById("booking_first_name").value=name;
 });
+
+self.port.on("last-name", function(lastname){
+	document.getElementById("booking_last_name").value=lastname;
+});
--- data/pop-over.js
+++ data/pop-over.js
@@ -1,5 +1,8 @@
 var button = document.getElementById("pop-over");
 
 button.addEventListener('click', function (event) {
-	self.port.emit("add-name", "Cameron");
+	self.port.emit("add-name", {
+		name: "Cameron", 
+		lastname: "bradley"
+	});
 }, false);
