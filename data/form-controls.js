6ca5dd63cea8ce080b03bfeec0f778cc5429f360
Cameron Bradley <cameron@hooroo.com>
2014-08-19 16:51:00 +1000

    finished basic book now functionality - ids/class

--- data/form-controls.js
+++ data/form-controls.js
@@ -5,3 +5,47 @@ self.port.on("first-name", function(firstname){
 self.port.on("last-name", function(lastname){
 	document.getElementById("booking_last_name").value=lastname;
 });
+
+self.port.on("email", function(email){
+	document.getElementById("booking_email").value=email;
+});
+
+self.port.on("phone-number", function(phone){
+	document.getElementById("booking_phone").value=phone;
+});
+
+self.port.on("qff-number", function(qffnumber){
+	document.getElementById("booking_frequent_flyer_number").value=qffnumber;
+});
+
+self.port.on("request", function(request){
+	document.getElementById("booking_special_requests").value=request;
+});
+
+self.port.on("credit-card-number", function(creditcardnumber){
+	document.getElementById("booking_credit_card_number").value=creditcardnumber;
+});
+
+self.port.on("credit-card-name", function(creditcardname){
+	document.getElementById("booking_credit_card_name").value=creditcardname;
+});
+
+self.port.on("credit-card-ccv", function(creditcardccv){
+	document.getElementById("booking_credit_card_verification_value").value=creditcardccv;
+});
+
+self.port.on("credit-card-month-expiry", function(creditcardmonthexpiry){
+	document.getElementById("month-button").getElementsByClassName("ui-selectmenu-status")[0].innerHTML=creditcardmonthexpiry
+});
+
+self.port.on("credit-card-year-expiry", function(creditcardyearexpiry){
+	document.getElementById("year-button").getElementsByClassName("ui-selectmenu-status")[0].innerHTML=creditcardyearexpiry
+});
+
+self.port.on("terms-and-conditions", function(termsandconditions){
+	document.getElementById("terms_and_conditions").checked=termsandconditions;
+});
+
+self.port.on("pay-now", function(){
+  document.getElementById("pay").click();
+});
--- data/pop-over.js
+++ data/pop-over.js
@@ -3,6 +3,16 @@ var button = document.getElementById("pop-over");
 button.addEventListener('click', function (event) {
 	self.port.emit("fill-booking-form", {
 		firstname: "Cameron", 
-		lastname: "Bradley"
+		lastname: "Bradley",
+		email: "cameron@hooroo.com",
+		phonenumber: "1300 738 214",
+		qffnumber: "9900029",
+		request: "Booking by Hooroomatic! (Profile: default)",
+		creditcardnumber: "5577810000000004",
+		creditcardname: "Cameron Bradley",
+		creditcardccv: "100",
+		creditcardmonthexpiry: "10",
+		creditcardyearexpiry: "2020",
+		termsandconditions: "true"
 	});
 }, false);
